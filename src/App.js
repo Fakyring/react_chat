fe70dc0f3ac042cf8c5b26b2a47c78b2d58e7586
Fakyring <darkveyron@mail.ru>
2024-02-19 02:44:28 +0300

    Added footer component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from "./components/footer";
 
 function App() {
     let insertData = "Астахов Никита Иванович Борисов Денис Александрович Вдовичев Никита Сергеевич Жирнов Никита Игоревич Заикин Даниил Юрьевич Зворыкин Глеб Игоревич Козлова Мария Евгеньевна Крутиков Никита Игоревич Лосев Антон Сергеевич Макарова Александра Михайловна Муратов Тимур Шухратович Назаренко Екатерина Михайловна Никитин Артём Александрович Новоселова Татьяна Михайловна Орлов Данил Дмитриевич Пискун Дмитрий Михайлович Проходцев Сергей Олегович Рахманов Амир Ренатович Редков Максим Александрович Романенко Андрей Георгиевич Рыжков Александр Сергеевич Семенов Александр Владимирович Семянников Никита Сергеевич Степанов Константин Константинович Чеснаков Максим Михайлович Чикилев Данил Дмитриевич Чуркин Максим Евгеньевич Широков Антон Дмитриевич Шрестха Алекс Раджинович"
     insertData = insertData.split(' ')
     let users = []
-    for (let i = 0; i <= insertData.length; i += 3) {
-        users.push({'surname': insertData[i], 'name': insertData[i + 1], 'patronymic': insertData[i + 2]})
+    for (let i = 0; i < insertData.length; i += 3) {
+        users.push({'id': (i / 3) + 1, 'surname': insertData[i], 'name': insertData[i + 1], 'patronymic': insertData[i + 2]})
     }
     console.log(users)
     return (
         <div className="container">
             <div className="chats">
+                <input type={"checkbox"} className={"check"}></input>
                 <table>
+                    <caption>Ст*денты</caption>
                     <tr>
+                        <th>ID</th>
                         <th>Фамилия</th>
                         <th>Имя</th>
                         <th>Отчество</th>
@@ -21,6 +24,7 @@ function App() {
                     {users.map(user => {
                         return (
                             <tr>
+                                <td>{user.id}</td>
                                 <td>{user.surname}</td>
                                 <td>{user.name}</td>
                                 <td>{user.patronymic}</td>
@@ -29,6 +33,7 @@ function App() {
                     })}
                 </table>
             </div>
+            <Footer/>
         </div>
     );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+function Footer (){
+    return(
+        <footer>
+            <div className='media'><img src='https://cdn4.iconfinder.com/data/icons/social-circle/512/vk-512.png' alt='N/A'/><a rel="noreferrer" target='_blank' href='https://vk.com/fakyring'> </a></div>
+            <div className='media'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Circle-icons-mail.svg/1024px-Circle-icons-mail.svg.png' alt='N/A'/><a rel="noreferrer" target='_blank' href='mailto:darkveyron@mail.ru'> </a></div>
+            <div className='media'><img src='https://upload.wikimedia.org/wikipedia/ru/2/2d/MIREA_Logo.svg' alt='N/A'/><a rel="noreferrer" target='_blank' href='https://mirea.ru'> </a></div>
+            <div className='media'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Steam_icon_logo.svg/2048px-Steam_icon_logo.svg.png' alt='N/A'/><a rel="noreferrer" target='_blank' href='https://steamcommunity.com/id/fakyring/'> </a></div>
+        </footer>
+    )
+}
+export default Footer;
