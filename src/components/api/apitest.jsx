2d21f9933ca14080b0177750b2972eb04247c1ea
Fakyring <darkveyron@mail.ru>
2024-03-25 12:33:02 +0300

    Changed api

--- src/components/api/apitest.jsx
+++ src/components/api/apitest.jsx
@@ -10,14 +10,14 @@ function Apitest() {
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({login: user.login, password: user.password})
         };
-        fetch('/rapi/api/v1/login', requestOptions)
+        fetch('https://fakereactchat.netlify.app/rapi/api/v1/login', requestOptions)
             .then(response => response.json())
             .then(data => localStorage.setItem('token', data.access_token));
     }
 
     function getUsers() {
         const headers = {'Authorization': 'Bearer ' + localStorage.getItem('token')}
-        fetch('/rapi/api/v1/users', {headers})
+        fetch('https://fakereactchat.netlify.app/rapi/api/v1/users', {headers})
             .then(response => response.json())
             .then(data => setUsers(data.data));
     }
